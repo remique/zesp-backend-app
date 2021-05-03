bc95c151bb4e500bb329717f06a489c95984dc71
ejaszczuk <emj1054@gmail.com>
2021-05-03 20:29:53 +0200

    Add only_me field in GET /activity

--- resources/activities.py
+++ resources/activities.py
@@ -22,10 +22,35 @@ class ActivitiesApi(Resource):
             '200': {
                 'description': 'Successfully got all the activities',
             }
-        }
+        },
+        'parameters': [
+            {
+                'name': 'only_me',
+                'in': 'query',
+                'type': 'boolean',
+                'description': '*Optional*: Filter by logged in user only'
+            }
+        ],
+        'security': [
+            {
+                'api_key': []
+            }
+        ]
     })
+    @jwt_required()
     def get(self):
         """Return ALL the activities"""
+        claims = get_jwt()
+        current_user_id = claims['id']
+
+        only_me_query = request.args.get('only_me')
+
+        if only_me_query == 'true':
+            user_activities = Activity.query\
+                .filter(Activity.user_id == current_user_id).first()
+            result = activity_schema.dump(user_activities)
+            return jsonify(result)
+
         all_activities = Activity.query.all()
         result = activities_schema.dump(all_activities)
         return jsonify(result)
@@ -126,14 +151,15 @@ class GroupActivitiesApi(Resource):
             return jsonify({'msg': 'Group doesnt exist'})
 
         activities = Activity.query.all()
-        #Get users from user institution
-        users = User.query.filter(User.institution_id == current_user_inst_id).all()
-        #Get users with given role
+        # Get users from user institution
+        users = User.query.filter(
+            User.institution_id == current_user_inst_id).all()
+        # Get users with given role
         users = list(filter(lambda x: role in x.roles, users))
-        #Get users with given group
+        # Get users with given group
         users = list(filter(lambda x: group in x.groups, users))
 
-        #Get activities
+        # Get activities
         for u in activities:
             user = list(filter(lambda x: x.id == u.user_id, users))
             if(user):
